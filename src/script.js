566c52fec1a41883fdf27717a74cfc5c314d7f96
Charles Larroca <larroca_1000@hotmail.com>
2024-04-23 22:41:25 -0300

    .

--- src/script.js
+++ src/script.js
@@ -1,1 +1,7 @@
 // write your JavaScript here
+document.addEventListener('click', function(e){
+  let el = (e.target.tagName === 'BUTTON') ? e.target.value : 'Não localizado'
+  console.log(el)
+})
+
+
